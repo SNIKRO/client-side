0fc9dfa118601ea6bf24272e3b565cc227fc06ea
SNIKR <snikr@bk.ru>
2022-06-03 16:34:37 +0600

    use boostrap nav bar

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -2,30 +2,32 @@ import Link from 'next/link'
 import styles from '../styles/mainLayout.module.css'
 import Image from 'next/image'
 import logo from '../images/logoOmGTU.png'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 
 export default function MainLayout({ children }) {
   return (
     <>
     <Navbar collapseOnSelect expanded="lg" bg="dark" variant='dark'>
-      <Navbar.Brand>
-        <Image
-          className={`d-line-block align-top ${styles.image}`}
-          src={ logo }
-          alt="Логотип ОмГТУ"
-          height={55}
-          width={ 150 }
-        />
-         Информатика и вычислительная техника
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-      <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='mr-auto'>
-          <Nav.Link href='/'>ГЛАВНАЯ СТРАНИЦА</Nav.Link>
-          <Nav.Link href='/about'>О НАС</Nav.Link>
-        </Nav>  
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand>
+          <Image
+            className={`d-line-block align-top ${styles.image}`}
+            src={ logo }
+            alt="Логотип ОмГТУ"
+            height={55}
+            width={ 150 }
+          />
+          Информатика и вычислительная техника
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='/'>ГЛАВНАЯ СТРАНИЦА</Nav.Link>
+            <Nav.Link href='/about'>О НАС</Nav.Link>
+          </Nav>  
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
       <main className={ styles.main }>
         {children}
